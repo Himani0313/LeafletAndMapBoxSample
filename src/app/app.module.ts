93f94daad09d3ff9a8ee92e48d84b4b9871ba14c
Himani Shah <hjshah2@asu.edu>
2018-01-25 12:52:19 -0700

    trail drawn with mapbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
-import {RouterModule} from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import {ActivityListComponent} from './activity-list/activity-list.component';
-import {MapComponent} from './map/map.component';
+import { ActivityListComponent } from './activity-list/activity-list.component';
+import { MapComponent } from './map/map.component';
 
-import {ActivityService} from './services/activity.service';
-import {MapService} from './services/map.service';
-import {appRoutes} from '../routes';
+import { ActivityService } from './services/activity.service';
+import { MapService } from './services/map.service';
+import { appRoutes } from '../routes';
 
 @NgModule({
   declarations: [
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  MAPBOX_API_KEY: 'pk.eyJ1IjoiaGltYW5pMzEzIiwiYSI6ImNqY3V3ZjBvdjBlbjQycW11eDdyem11dnMifQ.yqjbj4rbuYIByGQ9EXn2IA'
 };
